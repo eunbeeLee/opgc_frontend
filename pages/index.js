7dd7e0d0b62500658bfbb7234fd20456844e4864
Chun <ginameeee@gmail.com>
2020-08-20 17:44:53 +0900

    create next app

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>OPGC</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Over Powered Good Coding!</h1>
+      <h2>Made By DirtyBoyz</h2>
+    </div>
+  );
+}
