c635c376723c246047a8ac151387cd73163df784
Chun <ginameeee@gmail.com>
2020-08-23 23:08:23 +0900

    styled-components 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,19 @@
 import Head from "next/head";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: yellow;
+`;
 
 export default function Home() {
   return (
-    <div className="container">
+    <Container>
       <Head>
         <title>OPGC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Over Powered Good Coding!</h1>
       <h2>Made By DirtyBoyz</h2>
-    </div>
+    </Container>
   );
 }
