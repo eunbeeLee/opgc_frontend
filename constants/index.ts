cdd830db2689365615eea3321ff125b050bfc3dd
Chun <ginameeee@gmail.com>
2020-09-13 13:12:18 +0900

    레이아웃 구성

---
+++ constants/index.ts
@@ -0,0 +1,10 @@
+type Menu = {
+  title: string;
+  path: string;
+};
+
+export const MENU_LIST: Menu[] = [
+  { title: "main", path: "/" },
+  { title: "menu 1", path: "/1" },
+  { title: "menu 2", path: "/2" },
+];
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import 'antd/dist/antd.css'
+import MainLayout from "../layouts/MainLayout";
+import "antd/dist/antd.css";
 
-function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Component {...pageProps} />
-        </>
-    );
+function App({ Component, pageProps }) {
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
 }
 
-export default MyApp;
+export default App;
