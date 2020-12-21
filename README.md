832019e84a89c1d9b26a1d0975748cf7f9d30976
Jangchun Lee <adama@wemakeprice.com>
2020-12-22 00:43:04 +0900

    redux devtools 적용

---
+++ README.md
@@ -0,0 +1,3 @@
+# opgc_frontend
+
+Over Programmed Good Coding 프론트엔드 레포지토리
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '@/modules';
 import App from '@/App';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
     <Provider store={store}>
