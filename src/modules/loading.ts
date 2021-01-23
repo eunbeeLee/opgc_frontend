0e712ce15e6dac51b43a345fc85a40c2c23ec895
Jangchun Lee <adama@wemakeprice.com>
2021-01-23 23:55:37 +0900

    loading redux module 생성

---
+++ src/modules/loading.ts
@@ -0,0 +1,32 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const START_LOADING = 'loading/START_LOADING';
+const FINISH_LOADING = 'loading/FINISH_LOADING';
+
+export const startLoading = createAction(
+    START_LOADING,
+    (requestType: string): string => requestType
+);
+
+export const finishLoading = createAction(
+    FINISH_LOADING,
+    (requestType: string): string => requestType
+);
+
+const initialState = {};
+
+const loading = handleActions(
+    {
+        [START_LOADING]: (state, action: { type: typeof START_LOADING, payload: string }) => ({
+            ...state,
+            [action.payload]: true
+        }),
+        [FINISH_LOADING]: (state, action: { type: typeof FINISH_LOADING, payload: string }) => ({
+            ...state,
+            [action.payload]: false
+        }),
+    }, 
+    initialState
+);
+
+export default loading;
