c30fc000dc9e1167e8d286504368374e6ec4e0c1
Jangchun Lee <adama@wemakeprice.com>
2021-01-24 03:27:17 +0900

    User menu 가림

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -1,7 +1,7 @@
 import { SearchPage, RankingPage, UserPage } from '@/pages';
 
-export const MAIN_MENU_LIST = [
-    { display: 'Search', name: 'search', path: '/main/search', component: SearchPage },
-    { display: 'Ranking', name: 'ranking', path: '/main/ranking', component: RankingPage },
-    { display: 'User', name: 'user', path: '/main/user/:userId', component: UserPage },
+export const MAIN_MENU_LIST: IMenu[] = [
+    { display: 'Search', name: 'search', path: '/main/search', component: SearchPage, visible: true },
+    { display: 'Ranking', name: 'ranking', path: '/main/ranking', component: RankingPage, visible: true },
+    { display: 'User', name: 'user', path: '/main/user/:userId', component: UserPage, visible: false },
 ];
---
+++ src/types/index.d.ts
@@ -0,0 +1,7 @@
+interface IMenu {
+    display: string;
+    name: string;
+    path: string;
+    visible: boolean;
+    component: React.FC<any>
+}
