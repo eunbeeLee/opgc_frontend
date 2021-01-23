0192b3aae84260b57cdb001b94a960c613efe300
Jangchun Lee <adama@wemakeprice.com>
2021-01-24 03:11:55 +0900

    user path URL 파라미터 처리

--- src/constants/menu.tsx
+++ src/constants/menu.tsx
@@ -3,5 +3,5 @@ import { SearchPage, RankingPage, UserPage } from '@/pages';
 export const MAIN_MENU_LIST = [
     { display: 'Search', name: 'search', path: '/main/search', component: SearchPage },
     { display: 'Ranking', name: 'ranking', path: '/main/ranking', component: RankingPage },
-    { display: 'User', name: 'user', path: '/main/user', component: UserPage },
+    { display: 'User', name: 'user', path: '/main/user/:userId', component: UserPage },
 ];
