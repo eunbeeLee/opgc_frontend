e0b8545748bc2251b8a0cc81d91f5adc192e87e1
Jangchun Lee <adama@wemakeprice.com>
2021-01-24 01:35:57 +0900

    error redux module 생성/적용

--- src/libs/redux.ts
+++ src/libs/redux.ts
@@ -1,5 +1,6 @@
 import { call, put } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '@/modules/loading';
+import { setError } from '@/modules/error';
 
 /**
  * api request를 처리하는 saga를 리턴한다.
@@ -15,11 +16,13 @@ export function createRequestSaga (type: string, request) {
 
         try {
             const response = yield call(request, action.payload);
+            yield put(setError(null));
             yield put({
                 type: SUCCESS,
                 payload: response.data
             })
         } catch (e) {
+            yield put(setError(e));
             yield put({
                 type: FAILURE,
                 payload: e,
@@ -32,7 +35,7 @@ export function createRequestSaga (type: string, request) {
 }
 
 /**
- * API(or 비동기연산)에 필요한 ACTION TYPE들을 (type, success, failure ) return 한다.
+ * API(or 비동기연산)에 필요한 ACTION TYPE들을 (type, success, failure) return 한다.
  * @param type 
  */
 export function createRequestActionTypes (type: string): string[] {
