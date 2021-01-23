7e94a046e407e23a2580b29c331961a891ae0c23
Jangchun Lee <adama@wemakeprice.com>
2021-01-24 00:47:38 +0900

    libs/saga -> libs/redux 로 이름 변경, api관련 action type 정의함수 추가

--- src/libs/redux.ts
+++ src/libs/redux.ts
@@ -1,6 +1,11 @@
 import { call, put } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '@/modules/loading';
 
+/**
+ * api request를 처리하는 saga를 리턴한다.
+ * @param type 
+ * @param request 
+ */
 export function createRequestSaga (type: string, request) {
     const SUCCESS = `${type}_SUCCESS`;
     const FAILURE = `${type}_FAILURE`;
@@ -24,4 +29,15 @@ export function createRequestSaga (type: string, request) {
             yield put(finishLoading(type));
         }
     }
+}
+
+/**
+ * API(or 비동기연산)에 필요한 ACTION TYPE들을 (type, success, failure ) return 한다.
+ * @param type 
+ */
+export function createRequestActionTypes (type: string): string[] {
+    const success = `${type}_SUCCESS`;
+    const failure = `${type}_FAILURE`;
+
+    return [type, success, failure];
 }
