b90cb598272b738fe1920c5033d5d61ced894b59
Jangchun Lee <adama@wemakeprice.com>
2021-01-30 00:29:24 +0900

    환경변수 적용

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,89 @@
+const webpack = require("webpack");
+const dotenv = require('dotenv');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
+
 const path = require('path');
 const srcPath = require('path').resolve(__dirname, 'src');
 const distPath = require('path').resolve(__dirname, 'dist');
 
-module.exports = {
-    name: 'opgc',
+module.exports = (env, options) => {
 
-    mode: 'development',
+    dotenv.config({
+        path: path.join(__dirname, `.env.${options.mode}`)
+    });
 
-    devtool: 'eval',
+    return {
+        name: 'opgc',
 
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        alias: {
-            '@': srcPath,
-            '~': srcPath,
-            '@@': srcPath,
-            '~~': srcPath,
-          },
-    },
+        mode: options.mode,
 
-    entry: {
-        app: [path.join(srcPath, '/index')]
-    },
+        devtool: 'eval',
 
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['@babel/preset-env', {
-                            targets: {
-                                browsers: ['last 2 chrome versions']
-                            }
-                        }], 
-                        '@babel/preset-react'
-                    ],
-                    plugins: [
-                        '@babel/plugin-proposal-class-properties',
-                        'react-refresh/babel'
-                    ]                
-                }
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            alias: {
+                '@': srcPath,
+                '~': srcPath,
+                '@@': srcPath,
+                '~~': srcPath,
             },
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-            { test: /\.css/, use: [ 'style-loader', 'css-loader' ]}
-        ]
-    },
+        },
 
-    plugins: [
-        new ReactRefreshWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(srcPath, '/index.html'),
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: path.resolve(srcPath, 'assets'), to: 'assets' }
-            ],
-        }),
-    ],
+        entry: {
+            app: [path.join(srcPath, '/index')]
+        },
 
-    output: {
-        path: distPath,
-        filename: 'app.js',
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?/,
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    browsers: ['last 2 chrome versions']
+                                }
+                            }], 
+                            '@babel/preset-react'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            'react-refresh/babel'
+                        ]                
+                    }
+                },
+                { test: /\.tsx?$/, loader: 'ts-loader' },
+                { test: /\.css/, use: [ 'style-loader', 'css-loader' ]}
+            ]
+        },
 
-    devServer: {
-        contentBase: path.join(distPath),
-        hot: true,
-        historyApiFallback: true,
-        port: 52203
+        plugins: [
+            new webpack.DefinePlugin({}),
+            new ReactRefreshWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: path.join(srcPath, '/index.html'),
+            }),
+            new CopyPlugin({
+                patterns: [
+                    { from: path.resolve(srcPath, 'assets'), to: 'assets' }
+                ],
+            }),
+        ],
+
+        output: {
+            path: distPath,
+            filename: 'app.js',
+        },
+
+        devServer: {
+            contentBase: path.join(distPath),
+            hot: true,
+            historyApiFallback: true,
+            port: 52203
+        }
     }
 };
