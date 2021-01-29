6be86d87b521dd31310c792c5b2bd3c314b2ed6d
Jangchun Lee <adama@wemakeprice.com>
2021-01-30 01:09:05 +0900

    eslint 설치

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    "overrides": [
+        {
+          "files": ["bin/*.js", "lib/*.js"],
+          "excludedFiles": "*.test.js",
+          "rules": {
+            "quotes": ["error", "single"]
+          }
+        }
+      ]
+      
+};
