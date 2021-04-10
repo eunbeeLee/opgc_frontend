1d60437dfb20dea0256c224dbf5b10ccaca81008
jay <ckj9014@gmail.com>
2021-04-11 00:00:02 +0900

    [components] main 디자인 수정, header 디자인 수정 (1일 1커밋 랭킹만 메인에 노출)

--- src/components/layouts/MainLayout/Header.tsx
+++ src/components/layouts/MainLayout/Header.tsx
@@ -4,28 +4,30 @@ import { NavLink, Link } from 'react-router-dom';
 const Header = ({ menuList }) => {
     return (
         <header id="header">
-            <Link to="/">
-                <h1 className="header__title">
-                    <img src="/assets/imgs/logo-white.png" />
-                    <span>OPGC</span>
-                </h1>
-            </Link>
+            <div className="header__warp">
+                <Link className="header__link" to="/">
+                    <h1 className="header__title">
+                        <img src="/assets/imgs/logo-white.png" />
+                        <span>OPGC</span>
+                    </h1>
+                </Link>
 
-            <nav className="header__navbar navbar">
-                <ul className="navbar__menu">
-                    {menuList.map((menu) => (
-                        <li key={menu.name}>
-                            <NavLink
-                                to={menu.path}
-                                className="navbar__item"
-                                activeClassName="active"
-                            >
-                                {menu.display}
-                            </NavLink>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+                <nav className="header__navbar navbar">
+                    <ul className="navbar__menu">
+                        {menuList.map((menu) => (
+                            <li key={menu.name}>
+                                <NavLink
+                                    to={menu.path}
+                                    className="navbar__item"
+                                    activeClassName="active"
+                                >
+                                    {menu.display}
+                                </NavLink>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
         </header>
     );
 };
