234482a64c9bf2223e10d7686e8ebc5591dbde98
jay <ckj9014@gmail.com>
2021-04-12 10:10:44 +0900

    [MainPage] 반응형 디자인 추가

--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -46,7 +46,7 @@ const SearchPage: React.FC<I_PROPS> = () => {
             </section>
 
             {/* 1일 1커밋 */}
-            <section>
+            <section className="main__rank">
                 <h1>1일 1커밋을 실천하는 개발자 랭킹 🌱</h1><br />
                 <div>좀 더 다양한 랭킹은 상단 Rank 탭을 이용하세요.</div>
                 <div className="ranking__content">
