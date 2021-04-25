47f062aa3e6da27bc3cbe9edb195111a1d27dce1
Jangchun Lee <adama@wemakeprice.com>
2021-04-25 14:56:37 +0900

    Userpage 반응형 작업

--- src/components/pages/UserPage/UserInfo/index.tsx
+++ src/components/pages/UserPage/UserInfo/index.tsx
@@ -9,7 +9,6 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { faGratipay } from '@fortawesome/free-brands-svg-icons';
 import React, { useMemo } from 'react';
-import { useMediaQuery } from 'react-responsive';
 import './style.css';
 
 interface I_PROPS {
@@ -53,181 +52,98 @@ const UserInfo: React.FC<I_PROPS> = ({ user }) => {
         [user]
     );
 
-    const isDesktopOrLaptop = useMediaQuery({
-        query: '(min-device-width: 769px)'
-    })
-
-    const isMobileDevice = useMediaQuery({
-        query: '(max-device-width: 769px)'
-    })
-
     return (
         user && (
-            <>
-                {/* 데스크탑 화면 */}
-                {
-                    isDesktopOrLaptop && <div className="user-info-account">
-                        <div className="user-info-account__profile">
+            <div className="user-info-account">
+                <div className="user-info-account__profile">
+                    <a href={user.githubUrl} target="_blank" rel="noreferrer">
+                        <Avatar
+                            type={E_ROUND_TYPE.RECTANGLE}
+                            imgUrl={
+                                user.profileImgUrl || '/assets/imgs/logo.png'
+                            }
+                            width={170}
+                            height={170}
+                        />
+                    </a>
+                    <div className="user-info-account__profile__info">
+                        <ul className="user-info-account__values">
+                            {countInfo.map((count) => (
+                                <li
+                                    className="user-info-account__value"
+                                    key={count.name}
+                                >
+                                    <h3>
+                                        {count.icon && (
+                                            <FontAwesomeIcon
+                                                icon={count.icon}
+                                                className="user-info-account__value__icon"
+                                            />
+                                        )}
+                                        <span className="user-info-account__value__title">
+                                            {count.title}
+                                        </span>
+                                    </h3>
+                                    <span>{count.value}</span>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="user-info-account__name">
                             <a
                                 href={user.githubUrl}
                                 target="_blank"
                                 rel="noreferrer"
                             >
-                                <Avatar
-                                    type={E_ROUND_TYPE.RECTANGLE}
-                                    imgUrl={
-                                        user.profileImgUrl ||
-                                        '/assets/imgs/logo.png'
-                                    }
-                                    width={170}
-                                    height={170}
-                                />
+                                {user.name || user.username}
                             </a>
-                            <div className="user-info-account__profile__info">
-                                <ul className="user-info-account__values">
-                                    {countInfo.map((count) => (
-                                        <li
-                                            className="user-info-account__value"
-                                            key={count.name}
-                                        >
-                                            <h3>
-                                                {count.icon && (
-                                                    <FontAwesomeIcon
-                                                        icon={count.icon}
-                                                        className="user-info-account__value__icon"
-                                                    />
-                                                )}
-                                                <span className="user-info-account__value__title">
-                                                    {count.title}
-                                                </span>
-                                            </h3>
-                                            <span>{count.value}</span>
-                                        </li>
-                                    ))}
-                                </ul>
-                                <div className="user-info-account__name">
-                                    <a
-                                        href={user.githubUrl}
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        {user.name || user.username}
-                                    </a>
-                                    {user.name && (
-                                        <span className="user-info-account__id">
-                                            @{user.username}
-                                        </span>
-                                    )}
-                                </div>
-                                <div className="user-info-account__desc">
-                                    {user.desc}
-                                    <div className="user-info-account__belong">
-                                        {user.company && (
-                                            <div className="user-info-account__company">
-                                                <span className="user-info-account__belong-title">
-                                                    working at
-                                                </span>{' '}
-                                                {user.company}
-                                            </div>
-                                        )}
-                                        {user.organizations.length > 0 && (
-                                            <div className="user-info-account__organization">
-                                                <span className="user-info-account__belong-title">
-                                                    {' '}
-                                                    a member of
-                                                </span>{' '}
-                                                {user.organizations.map((o) => (
-                                                    <Avatar
-                                                        key={o.name}
-                                                        type={
-                                                            E_ROUND_TYPE.RECTANGLE
-                                                        }
-                                                        imgUrl={
-                                                            o.logoUrl ||
-                                                            '/assets/imgs/logo.png'
-                                                        }
-                                                        width={25}
-                                                        height={25}
-                                                        title={o.name}
-                                                    />
-                                                ))}
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                }
-            {/* 모바일 화면 */}
-            {isMobileDevice && <div className="user-info-account">
-                <div className="user-info-account__profile">
-                    <div className="user-info-account__profile__wrap">
-                        <a href={user.githubUrl} target="_blank">
-                            <Avatar
-                                type={E_ROUND_TYPE.RECTANGLE}
-                                imgUrl={ user.profileImgUrl || '/assets/imgs/logo.png' }
-                                width={170}
-                                height={170}
-                            />
-                        </a>
-                    </div>
-                    <div className="user-info-account__profile__info">
-                        <div className="user-info-account__name">
-                            <a href={user.githubUrl} target="_blank">{user.name || user.username}</a>
-                            {user.name && <div><span className="user-info-account__id">@{user.username}</span></div>}
+                            {user.name && (
+                                <span className="user-info-account__id">
+                                    @{user.username}
+                                </span>
+                            )}
                         </div>
                         <div className="user-info-account__desc">
-                            {user.desc}
-                            <div className="user-info-account__belong">
-                                {
-                                    user.company && (
+                            <span>{user.desc}</span>
+                            {(user.company ||
+                                user.organizations.length > 0) && (
+                                <div className="user-info-account__belong">
+                                    {user.company && (
                                         <div className="user-info-account__company">
-                                            <span className="user-info-account__belong-title">working at</span> {user.company}
+                                            <span className="user-info-account__belong-title">
+                                                working at
+                                            </span>{' '}
+                                            {user.company}
                                         </div>
-                                    )
-                                }
-                            </div>
+                                    )}
+                                    {user.organizations.length > 0 && (
+                                        <div className="user-info-account__organization">
+                                            <span className="user-info-account__belong-title">
+                                                {' '}
+                                                a member of
+                                            </span>{' '}
+                                            {user.organizations.map((o) => (
+                                                <Avatar
+                                                    key={o.name}
+                                                    type={
+                                                        E_ROUND_TYPE.RECTANGLE
+                                                    }
+                                                    imgUrl={
+                                                        o.logoUrl ||
+                                                        '/assets/imgs/logo.png'
+                                                    }
+                                                    width={25}
+                                                    height={25}
+                                                    title={o.name}
+                                                />
+                                            ))}
+                                        </div>
+                                    )}
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
-                <div className="member__wrap">
-                    {
-                        user.organizations.length > 0 && (
-                            <div className="user-info-account__organization">
-                                <span className="user-info-account__belong-title"> a member of</span> {
-                                    user.organizations.map(o => (
-                                        <Avatar
-                                            key={o.name}
-                                            type={E_ROUND_TYPE.RECTANGLE}
-                                            imgUrl={o.logoUrl || '/assets/imgs/logo.png'}
-                                            width={25}
-                                            height={25}
-                                            title={o.name}
-                                        />
-                                    ))
-                                }
-                            </div>
-                        )
-                    }
-                </div>
-                <div>
-                    <ul className="user-info-account__values">
-                        {
-                            countInfo.map((count) => (
-                                <li className="user-info-account__value" key={count.name}>
-                                    <h3>
-                                        {count.icon && <FontAwesomeIcon icon={count.icon} className="user-info-account__value__icon"/>}
-                                        <span className="user-info-account__value__title">{count.title}</span>
-                                    </h3>
-                                    <span>{count.value}</span>
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            </div>}
-        </>
+            </div>
         )
     );
 };
