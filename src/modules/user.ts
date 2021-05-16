3fce0bddfc9b33c5d3b368a3d5566f0036365ab5
Jangchun Lee <adama@wemakeprice.com>
2021-05-16 17:09:08 +0900

    user 404

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -13,23 +13,26 @@ interface I_STATE {
 /**
  * actions
  */
-export const GET_USER = 'user/GET_USER';
-export const GET_USER_SUCCESS = 'user/GET_USER_SUCCESS';
-export const GET_USER_FAILURE = 'user/GET_USER_FAILURE';
-
 export const PATCH_USER = 'user/PATCH_USER';
 export const PATCH_USER_SUCCESS = 'user/PATCH_USER_SUCCESS';
 export const PATCH_USER_FAILURE = 'user/PATCH_USER_FAILURE';
+
+export const UPDATE_USER = 'user/UPDATE_USER';
+export const UPDATE_USER_SUCCESS = 'user/UPDATE_USER_SUCCESS';
+export const UPDATE_USER_FAILURE = 'user/UPDATE_USER_FAILURE';
 
 /**
  * functions for creating actions
  */
 export const actions = {
+    updateUser: createAction(
+        UPDATE_USER,
+        (username: string): string => username
+    ),
     patchUser: createAction(PATCH_USER, (username: string): string => username),
-    getUser: createAction(GET_USER, (username: string): string => username),
-    getUserSuccess: createAction(GET_USER_SUCCESS, (user: User) => user),
-    getUserFailure: createAction(
-        GET_USER_FAILURE,
+    patchUserSuccess: createAction(PATCH_USER_SUCCESS, (user: User) => user),
+    patchUserFailure: createAction(
+        PATCH_USER_FAILURE,
         (error: Error): Error => error
     ),
 };
@@ -44,15 +47,15 @@ const initialState: I_STATE = {
 /**
  * action saga
  */
-const getUserSaga = createRequestSaga(GET_USER, api.getUser);
-const patchUserSaga = createRequestSaga(PATCH_USER, api.patchUser);
+const patchUserSaga = createRequestSaga(PATCH_USER, api.getUser);
+const updateUserSaga = createRequestSaga(UPDATE_USER, api.patchUser);
 
 /**
  * module saga
  */
 export function* userSaga(): Generator {
-    yield takeLatest(GET_USER, getUserSaga);
     yield takeLatest(PATCH_USER, patchUserSaga);
+    yield takeLatest(UPDATE_USER, updateUserSaga);
 }
 
 /**
@@ -60,19 +63,6 @@ export function* userSaga(): Generator {
  */
 const user = handleActions(
     {
-        [GET_USER_SUCCESS]: (
-            state: I_STATE,
-            { payload }: { payload: User }
-        ): I_STATE => ({
-            ...state,
-            user: payload,
-        }),
-        [GET_USER_FAILURE]: (
-            state: I_STATE /*{ payload }: { payload: Error }*/
-        ): any => ({
-            ...state,
-            user: null,
-        }),
         [PATCH_USER_SUCCESS]: (
             state: I_STATE,
             { payload }: { payload: User }
@@ -81,6 +71,19 @@ const user = handleActions(
             user: payload,
         }),
         [PATCH_USER_FAILURE]: (
+            state: I_STATE /*{ payload }: { payload: Error }*/
+        ): any => ({
+            ...state,
+            user: null,
+        }),
+        [UPDATE_USER_SUCCESS]: (
+            state: I_STATE,
+            { payload }: { payload: User }
+        ): I_STATE => ({
+            ...state,
+            user: payload,
+        }),
+        [UPDATE_USER_FAILURE]: (
             state: I_STATE
             /*{ payload }: { payload: Error }*/
         ): any => ({
