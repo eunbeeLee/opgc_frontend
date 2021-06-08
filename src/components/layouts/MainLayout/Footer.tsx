1f66eada8ee460e4e7190bd891eea22f50fdb582
ginameee <ginameeee@gmail.com>
2021-06-09 00:57:45 +0900

    푸터 디자인 변경 및 노션 연결

--- src/components/layouts/MainLayout/Footer.tsx
+++ src/components/layouts/MainLayout/Footer.tsx
@@ -6,19 +6,33 @@ const Footer = () => {
     return (
         <footer id="footer">
             <p className="footer__desc">
-                Copyright© 2020-{nowYear} OPGC
-                <br />
-                Over Programmed Good Coding
+                Copyright© 2020-{nowYear} Dirty Boyz
+                <img src="/assets/imgs/logo.png" />
             </p>
+            <div className="footer__spliter pc-only"></div>
             <ul className="footer__links">
                 <li className="footer__links__item">
-                    <a>
-                        <i className="fab fa-github-square"></i>
+                    <a
+                        href="https://www.notion.so/dirtyboyz/About-1646eb48c3264fbd915bbe2779f49845"
+                        target="_blank"
+                    >
+                        About
                     </a>
                 </li>
                 <li className="footer__links__item">
-                    <a>
-                        <i className="far fa-envelope"></i>
+                    <a
+                        href="https://www.notion.so/dirtyboyz/Contact-e17a6504178146e7b2e863ecd6463414"
+                        target="_blank"
+                    >
+                        Contact
+                    </a>
+                </li>
+                <li className="footer__links__item">
+                    <a
+                        href="https://www.notion.so/dirtyboyz/Private-Policy-2af612a45efe45198fdbd74c67de0160"
+                        target="_blank"
+                    >
+                        Private Policy
                     </a>
                 </li>
             </ul>
